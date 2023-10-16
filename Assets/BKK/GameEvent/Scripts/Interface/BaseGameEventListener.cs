5fbbcda146ea1049d68b963621a96839181e6f67
Kokyung Byun <kikikg6@naver.com>
2023-10-16 20:12:02 +0900

    제네릭 타입 게임 이벤트 리스너 매니저 윈도우 표시 작업 진행중

--- Assets/BKK/GameEvent/Scripts/Interface/BaseGameEventListener.cs
+++ Assets/BKK/GameEvent/Scripts/Interface/BaseGameEventListener.cs
@@ -2,27 +2,32 @@ using UnityEngine;
 
 namespace BKK.GameEventArchitecture
 {
-    public abstract class BaseGameEventListener : MonoBehaviour
+    public abstract class BaseGameEventListener : EventListenerBehaviour
     {
         [Tooltip("등록할 게임 이벤트"), SerializeField]
         public BaseGameEvent gameEvent;
         
+        public new GameEventAsset GameEventAsset => gameEvent;
+        
         public abstract void RaiseEvent();
 
         public abstract void StopEvent();
-
-        public abstract string GetListenerPath();
     }
     
-    public abstract class BaseGameEventListener<Ttype> : MonoBehaviour
+    public abstract class BaseGameEventListener<Ttype> : EventListenerBehaviour
     {
         [Tooltip("등록할 게임 이벤트"), SerializeField]
         public BaseGameEvent<Ttype> gameEvent;
+
+        public new GameEventAsset GameEventAsset => gameEvent;
         
         public abstract void RaiseEvent(Ttype value);
 
         public abstract void StopEvent(Ttype value);
+    }
 
+    public abstract class EventListenerBehaviour : MonoBehaviour
+    {
         public abstract string GetListenerPath();
     }
 }
